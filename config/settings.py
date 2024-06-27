3b562a8f5100f9d3d9de2914d11155846af590ba
eugen <eugenschmigel@gmail.com>
2024-06-27 22:17:05 +0200

    homework 26.1 init

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
 
     'rest_framework',
     'course',
     'users',
+    'payment',
     'django_filters',
     'rest_framework_simplejwt',
 
